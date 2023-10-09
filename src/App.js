3e8587ef3e6144c4ef500f0f6263b73e6f8e1c99
lucasdsm78 <dasilvamarqueslucas@gmail.com>
2023-10-09 15:30:49 +0200

    push final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
+
 
 function App() {
   return (
--- src/BackgroundImage.js
+++ src/BackgroundImage.js
@@ -1,66 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import './BackgroundImage.css';
 import axios from 'axios';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function BackgroundImage() {
     const [backgrounds, setBackgrounds] = useState([]);
+    const [treasures, setTreasures] = useState([]);
     const [newBackground, setNewBackground] = useState({ link: '', width: 0, height: 0 });
-    const [newTreasure, setNewTreasure] = useState({ file: '', score: 0 });
+    const [newTreasure, setNewTreasure] = useState({ image: '', score: 0 });
     const [selectedBackground, setSelectedBackground] = useState(null);
-    const backendUrl = process.env.BACKEND_URL;
-    console.log(backendUrl);
-    console.log("here");
 
     useEffect(() => {
-        axios.get(`${backendUrl}/background`)
-            .then((response) => {
-                setBackgrounds(response.data);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        fetchBackgrounds();
+        fetchTreasures();
     }, []);
 
-    const addBackground = async () => {
+    const fetchTreasures = async () => {
         try {
-            console.log(backendUrl);
-            console.log("here");
-            const formData = new FormData();
-            formData.append('link', newBackground.link);
-            formData.append('width', newBackground.width);
-            formData.append('height', newBackground.height);
-
-            const response = await axios.post(`${backendUrl}/background`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            setBackgrounds([...backgrounds, response.data]);
-            setNewBackground({ link: '', width: 0, height: 0 });
-
+            const response = await axios.get(`${backendUrl}/treasures`);
+            setTreasures(response.data);
         } catch (error) {
             console.error(error);
         }
     };
 
-    const addTreasure = async (e) => {
-        e.preventDefault();
+    const fetchBackgrounds = async () => {
         try {
-            console.log(backendUrl);
-            console.log("here");
-            const formData = new FormData();
-            formData.append('image', newTreasure.file.name);
-            formData.append('score', newTreasure.score);
+            const response = await axios.get(`${backendUrl}/backgrounds`);
+            setBackgrounds(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-            const response = await axios.post(`${backendUrl}/treasure`, formData, {
+    const addBackground = async () => {
+        try {
+            console.log("here");
+            const requestData = {
+                link: newBackground.link,
+                height: parseInt(newBackground.height, 10),
+                width: parseInt(newBackground.width, 10),
+            };
+
+            const response = await axios.post(`${backendUrl}/background`, requestData, {
                 headers: {
-                    'Content-Type': 'multipart/form-data',
+                    'Content-Type': 'application/json',
                 },
             });
             console.log(response);
 
-            setNewTreasure({ file: '', score: 0 });
+            setBackgrounds([...backgrounds, response.data]);
+            setNewBackground({ link: '', width: 0, height: 0 });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const selectBackground = (background) => {
+        setSelectedBackground(background);
+    };
+
+    const addTreasure = async (e) => {
+        e.preventDefault();
+        try {
+            const requestData = {
+                image: newTreasure.image,
+                score: parseInt(newTreasure.score, 10),
+            };
+
+            const response = await axios.post(`${backendUrl}/treasure`, requestData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            console.log(response);
+
+            setNewTreasure((prevState) => ({ ...prevState, link: '', score: 0 }));
 
         } catch (error) {
             console.log(error);
@@ -69,7 +85,7 @@ function BackgroundImage() {
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
-        setNewBackground({ ...newBackground, file });
+        setNewTreasure({ ...newTreasure, file });
     };
 
     const deleteBackground = async (backgroundId) => {
@@ -79,22 +95,61 @@ function BackgroundImage() {
             const updatedBackgrounds = backgrounds.filter((background) => background.id !== backgroundId);
             setBackgrounds(updatedBackgrounds);
         } catch (error) {
-            console.error(error);
         }
     };
 
-    const selectBackground = (background) => {
-        setSelectedBackground(background);
+    const deleteTreasure = async (treasureId) => {
+        try {
+            await axios.delete(`${backendUrl}/treasure/${treasureId}`);
+
+            const updateTreasures = treasures.filter((treasure) => treasure.id !== treasureId);
+            setTreasures(updateTreasures);
+        } catch (error) {
+        }
     };
 
     return (
         <div className="background-container">
-            {/* Affichez la liste des images de fond */}
+            <h1>Liste des backgrounds images</h1>
+                <ul>
+                    {backgrounds.map((background) => (
+                        <li key={background.id}>
+                            <img src={background.link} alt={`Background ${background.id}`} />
+                            <button onClick={() => deleteBackground(background.id)}>Supprimer</button>
+                        </li>
+                    ))}
+                </ul>
+
+            {/* ADD BACKGROUND */}
+            <form onSubmit={addBackground}>
+                <input
+                    type="text"
+                    placeholder="Link du background image"
+                    value={newBackground.link}
+                    onChange={(e) => setNewBackground({ ...newBackground, link: e.target.value })}
+                />
+                <input
+                    type="number"
+                    placeholder="Width du background"
+                    value={newBackground.width}
+                    onChange={(e) => setNewBackground({ ...newBackground, width: e.target.value })}
+                />
+                <input
+                    type="number"
+                    placeholder="Height du background"
+                    value={newBackground.height}
+                    onChange={(e) => setNewBackground({ ...newBackground, height: e.target.value })}
+                />
+                <button type="submit">Ajouter un background</button>
+            </form>
+
+            <h1>Liste des treasures</h1>
             <ul>
-                {backgrounds.map((background) => (
-                    <li key={background.id}>
-                        <img src={background.link} alt={`Background ${background.id}`} />
-                        <button onClick={() => deleteBackground(background.id)}>Supprimer</button>
+                {treasures.map((treasure) => (
+                    <li key={treasure.id}>
+                        <img src={treasure.image} alt={`Treasure ${treasure.id}`} />
+                        <p>Score: {treasure.score}</p>
+                        <button onClick={() => deleteTreasure(treasure.id)}>Supprimer</button>
                     </li>
                 ))}
             </ul>
@@ -102,9 +157,10 @@ function BackgroundImage() {
             {/* ADD TREASURE */}
             <form onSubmit={addTreasure}>
                 <input
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => setNewTreasure({ ...newTreasure, file: e.target.files[0] })}
+                    type="text"
+                    placeholder="Link du treasure"
+                    value={newTreasure.image}
+                    onChange={(e) => setNewTreasure({ ...newTreasure, image: e.target.value })}
                 />
                 <input
                     type="number"
@@ -113,16 +169,6 @@ function BackgroundImage() {
                     onChange={(e) => setNewTreasure({ ...newTreasure, score: e.target.value })}
                 />
                 <button type="submit">Ajouter un treasure</button>
-            </form>
-
-            {/* ADD BACKGROUND */}
-            <form onSubmit={addBackground}>
-                <input
-                    type="file" // Type "file" pour la sélection de fichier
-                    accept="image/*" // Pour limiter la sélection aux fichiers image
-                    onChange={handleFileChange} // Gérer le changement de fichier
-                />
-                <button type="submit">Ajouter un background</button>
             </form>
         </div>
     );
